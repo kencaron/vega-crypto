6037f647d545f4357811e73d535109de4cc3eeaa
Ken Caron <kcaron@centerstonetech.com>
2018-06-21 00:38:40 -0600

    typo

--- src/App.js
+++ src/App.js
@@ -93,7 +93,7 @@ class App extends Component {
             <p>
               This is a simple Preact app which uses the same API of React at a
               fraction of the size. It uses the CryptoCompare API and the chart
-              itself is rendered through Vega, which is built my much of the
+              itself is rendered through Vega, which is built by much of the
               same team behind D3.js.
             </p>
           </div>
